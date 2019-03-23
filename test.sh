a28cbc9bf4982d2d64b68f95bb9688424463e83f
Petr <xflajs00@stud.fit.vutbr.cz>
2019-03-23 17:06:29 +0100

    add test.sh

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function log2 {
+    local x=0
+    for (( y=$1-1 ; $y > 0; y >>= 1 )) ; do
+        let x=$x+1
+    done
+    echo ${x}
+}
+
+if [[ $# -lt 1 ]];then
+    numbers=10;
+else
+    numbers=$1;
+fi;
+
+dd if=/dev/random bs=1 count=${numbers} of=numbers
+
+if [[ ${numbers} -eq 1 ]];then
+    proc_count=1
+else
+    pw=$[$(log2 ${numbers})]
+    proc_count=$[2**pw - 1]
+fi;
+
+echo 'Numbers: '${numbers}
+echo 'Proc count: '${proc_count}
+
+mpic++ -std=c++17 --prefix /usr/local/share/openmpi -o PRL_1 bks.cpp
+
+mpirun --prefix /usr/local/share/openmpi -np ${proc_count} ./PRL_1
+
+rm -f numbers
